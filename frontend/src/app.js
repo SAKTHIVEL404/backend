8047035243d6d76e3e4e610d9423ef74d82d7ef8
sakthivel ganeshan <awssakthi404@gmail.com>
2024-12-07 11:30:56 +0530

    Add files via upload
    backend code

---
+++ frontend/src/app.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Jenkins Automation: Frontend</h1>
+      <p>This is a sample frontend for Jenkins automation.</p>
+    </div>
+  );
+}
+
+export default App;
